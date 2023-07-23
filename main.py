04b125e737f885c08194fa84e62001e04a7b6291
Sasanka <sajewsasanka1@gmail.com>
2023-07-23 14:44:19 +0530

    Update main.py
    - test the data base

--- main.py
+++ main.py
@@ -7,23 +7,59 @@ from textblob import TextBlob
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from tqdm import tqdm
+from scipy.special import softmax
+import mysql.connector as mc
+import os
+
+sql_database_config = {
+    'host': 'localhost',
+    'user': os.environ.get('SQL_USERNAME'),
+    'password': os.environ.get('SQL_PASSWORD'),
+    'database': 'moody'
+}
+
+mongo_database_config = {
+    'host': 'localhost',
+    'port': 27017,
+    'username': os.environ.get('MONGO__USERNAME'),
+    'password': os.environ.get('MONGO__PASSWORD'),
+}
 
 
-# connect to the mongoDB
-def connect_mongo():
-    client = pymongo.MongoClient('localhost', 27017)
-    db = client['test']
-    collection = db['test_collection']
-    return collection
+def get_data_from_sql():
+    conn = mc.connect(**sql_database_config)
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM reviews")
+
+    data = cursor.fetchall()
+
+    return data
+
+
+def mongo_data_schema():
+    client = pymongo.MongoClient(**mongo_database_config)
+    db = client.moody
+    collections = db.reviews
+
+    return collections
 
 
 def main():
-    # read the data from csv
-    df = pd.read_csv('data.csv')
-    # connect to the mongoDB
-    collection = connect_mongo()
-    # insert the data into mongoDB
-    collection.insert_many(df.to_dict('records'))
+    data = get_data_from_sql()
+    df = pd.DataFrame(data, columns=['id', 'product_id', 'review_title', 'review_comment', 'rating'])
+    print(df.head())
+
+    collections = mongo_data_schema()
+    sample_data =[
+        {"product_id": "1", "review_title": "title", "review_comment": "comment", "rating": 5},
+        {"product_id": "2", "review_title": "title", "review_comment": "comment", "rating": 5},
+    ]
+
+    collections.insert_many(sample_data)
+
+    print("Hello World")
 
 
 if __name__ == '__main__':
